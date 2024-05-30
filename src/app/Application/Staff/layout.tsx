37df56f93877bcbdbad521a99e255916825bee2b
BadWiX505 <houssamnaouali04@gmail.com>
2024-05-30 16:49:23 +0100

    V6

--- src/app/Application/Staff/layout.tsx
+++ src/app/Application/Staff/layout.tsx
@@ -1,24 +1,13 @@
 
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "@/app/utils/authOptions";
-import { getRole } from "@/Repo/Logic";
 export default async function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     
-    
-    const session = await getServerSession(authOptions);
-
-    if(session){
-      const res= await getRole({userName:session?.user?.name,email:session?.user?.email});
-       if(!res || res.role!=="staff"){
-        redirect("/login");
-       }
-    }    
-
+ 
     return (
       children
     );
