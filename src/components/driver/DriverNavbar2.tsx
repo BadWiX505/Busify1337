bcb531df4fd18ca9ed706460979e4fbae9bb03f4
BadWiX505 <houssamnaouali04@gmail.com>
2024-06-02 11:17:39 +0100

    V6

--- src/components/driver/DriverNavbar2.tsx
+++ src/components/driver/DriverNavbar2.tsx
@@ -167,6 +167,15 @@ export default function DriverNavbar2({driverInfo}) {
                 <WrenchIcon className="h-4 w-4" />
                 <span>Account</span>
               </DropdownMenuItem>
+
+              <DropdownMenuItem
+                className="flex items-center gap-2 "
+              >
+                <BusIcon className="h-4 w-4 bg-red-600 hover:bg-red-900" />
+                <span>Leave bus</span>
+              </DropdownMenuItem>
+
+
               <DropdownMenuItem className="flex items-center gap-2" onClick={destroySession}>
                 <SignOutIcon className="h-4 w-4" />
                 <span>Sign out</span>
@@ -210,6 +219,36 @@ export default function DriverNavbar2({driverInfo}) {
 }
 
 // Icons
+
+
+
+
+function BusIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M8 6v6" />
+      <path d="M15 6v6" />
+      <path d="M2 12h19.6" />
+      <path d="M18 18h3s.5-1.7.8-2.8c.1-.4.2-.8.2-1.2 0-.4-.1-.8-.2-1.2l-1.4-5C20.1 6.8 19.1 6 18 6H4a2 2 0 0 0-2 2v10h3" />
+      <circle cx="7" cy="18" r="2" />
+      <path d="M9 18h5" />
+      <circle cx="16" cy="18" r="2" />
+    </svg>
+  )
+}
+
+
 function AccidentIcon(props) {
   return (
     <svg
